26028ac29c8bbdef4c598edf21006e271a7dfa05
seiyaKM <seiya.kumada.0107@gmail.com>
2022-08-17 06:35:57 +0900

    first commit

---
+++ src/components/ToDo.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      todos: ["text A", "text B", "text C"],
+    });
+    return {
+      state,
+    };
+  },
+});
+</script>
+
+<template>
+  <section>
+    <label for="add-text">add item text</label>
+    <input type="text" id="add-text" />
+    <button>add</button>
+  </section>
+  <section>
+    <label for="delete-text">select delete items</label>
+    <button>delete</button>
+  </section>
+  <section>
+    <ul>
+      <li v-for="todo in state.todos" :key="index"></li>
+    </ul>
+  </section>
+</template>
+
+<style scoped></style>
